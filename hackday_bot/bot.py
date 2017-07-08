40e4605c5398cd0b3cc948e7a1bd2e9c1545ba69
Bryce Boe <bbzbryce@gmail.com>
2017-07-08 16:17:17 -0700

    Trivially save and load seen comments

--- hackday_bot/bot.py
+++ hackday_bot/bot.py
@@ -1,5 +1,7 @@
 """hackday_bot.bot module."""
+import json
 import logging
+import os
 import re
 import time
 
@@ -8,6 +10,8 @@ from prawcore.exceptions import PrawcoreException
 AVAILABLE_COMMANDS = ('help', 'interested', 'join', 'leave', 'uninterested')
 COMMAND_RE = re.compile(r'(?:\A|\s)!({})(?=\s|\Z)'
                         .format('|'.join(AVAILABLE_COMMANDS)))
+SEEN_COMMENT_PATH_TEMPLATE = os.path.join(os.environ['HOME'], '.config',
+                                          'hackday_bot_comments_{}.json')
 
 
 logger = logging.getLogger(__package__)
@@ -22,6 +26,8 @@ class Bot(object):
         :param subreddit: The subreddit to monitor for new comments.
         """
         self.subreddit = subreddit
+        self._seen_comment_path = SEEN_COMMENT_PATH_TEMPLATE.format(subreddit)
+        self._seen_comments = self._load_seen_comments()
 
     def _command_help(self, comment):
         comment.reply('help text will go here')
@@ -47,6 +53,21 @@ class Bot(object):
             getattr(self, '_command_{}'.format(command))(comment)
             logger.debug('Handled {} by {}'.format(command, comment.author))
 
+    def _load_seen_comments(self):
+        try:
+            with open(self._seen_comment_path) as fp:
+                data = set(json.load(fp))
+            logger.debug('Discovered {} seen comments'.format(len(data)))
+        except (FileNotFoundError, json.decoder.JSONDecodeError):
+            data = set()
+        return data
+
+    def _save_seen_comments(self):
+        with open(self._seen_comment_path, 'w') as fp:
+            json.dump(sorted(self._seen_comments), fp)
+        logger.debug('Recorded {} seen comments'
+                     .format(len(self._seen_comments)))
+
     def run(self):
         """Run the bot indefinitely."""
         running = True
@@ -56,11 +77,15 @@ class Bot(object):
         while running:
             try:
                 for comment in self.subreddit.stream.comments():
+                    if comment.id in self._seen_comments:
+                        continue
                     self._handle_comment(comment)
+                    self._seen_comments.add(comment.id)
             except KeyboardInterrupt:
                 logger.info('Termination received. Goodbye!')
                 running = False
             except PrawcoreException:
                 logger.exception('run loop')
                 time.sleep(10)
+        self._save_seen_comments()
         return 0
