6e0782f3c1c52f122b65fddb568e06c4a36e10e5
Bryce Boe <bbzbryce@gmail.com>
2017-07-08 17:31:31 -0700

    Add loading and saving project members to subreddit wiki page

--- hackday_bot/bot.py
+++ hackday_bot/bot.py
@@ -5,7 +5,7 @@ import os
 import re
 import time
 
-from prawcore.exceptions import PrawcoreException
+from prawcore.exceptions import NotFound, PrawcoreException
 
 AVAILABLE_COMMANDS = {
     'help': 'Output this help message.',
@@ -31,9 +31,10 @@ class Bot(object):
 
         :param subreddit: The subreddit to monitor for new comments.
         """
-        self.subreddit = subreddit
         self._seen_comment_path = SEEN_COMMENT_PATH_TEMPLATE.format(subreddit)
         self._seen_comments = self._load_seen_comments()
+        self.members = Members(subreddit)
+        self.subreddit = subreddit
 
     def _command_help(self, comment):
         table_rows = ['command|description',
@@ -99,3 +100,45 @@ class Bot(object):
                 time.sleep(10)
         self._save_seen_comments()
         return 0
+
+
+class Members(object):
+    """Keeps track of members of the various projects."""
+
+    def __init__(self, subreddit):
+        """Initialize and instance of Members.
+
+        :param subreddit: The subreddit for which projects are to be managed.
+
+        """
+        self._page = self._page = subreddit.wiki['projects']
+        try:
+            self.projects = self._load_projects()
+        except NotFound:
+            self._page = subreddit.wiki.create('projects', '')
+            self.projects = {}
+
+        self._save_projects('test')
+
+    def _load_projects(self):
+        projects = {}
+        url = None
+        for line in self._page.content_md.split('\n'):
+            line = line.strip()
+            if line.startswith('#'):
+                title, url = line[3:-1].split('](')
+                projects[url] = {'assignees': set(), 'title': title}
+            elif line.startswith('*'):
+                username = line.rsplit('/', 1)[1]
+                projects[url]['assignees'].add(username)
+        logger.debug('Loaded {} project(s)'.format(len(projects)))
+        return projects
+
+    def _save_projects(self, reason):
+        lines = []
+        for url, data in sorted(self.projects.items(),
+                                key=lambda x: x[1]['title']):
+            lines.append('# [{}]({})'.format(data['title'], url))
+            for member in sorted(data['assignees']):
+                lines.append('* /u/{}'.format(member))
+        self._page.edit('\n'.join(lines), reason=reason)
